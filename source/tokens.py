dd1985b845c3f49a86ad9be1f4a201adca678c60
vb64 <vit.sar68@gmail.com>
2026-02-25 22:14:54 +0400

    step

---
+++ source/tokens.py
@@ -0,0 +1,20 @@
+"""Tokens stuff."""
+
+
+class Tokenizer:
+    """Let there be a Tokenizer to translate strings to sequences of integers ("tokens") and back."""
+
+    def __init__(self, dataset):
+        """Create tokens for given dataset."""
+        # unique characters in the dataset become token ids 0..n-1
+        self.uchars = sorted(set(''.join(dataset.docs)))
+        self.bos = len(self.uchars)  # token id for a special Beginning of Sequence (BOS) token
+        self.size = self.bos + 1  # total number of unique tokens, +1 is for BOS
+
+    def __str__(self):
+        """Return string representation."""
+        return "vocab size: {}".format(self.size)
+
+    def tokenize(self, doc):
+        """Take single document, tokenize it, surround it with BOS special token on both sides."""
+        return [self.bos] + [self.uchars.index(ch) for ch in doc] + [self.bos]
