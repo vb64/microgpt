d86fccb4cf47a30512b9832f18c713ba4159220e
Vitaly Bogomolov <vit.sar68@gmail.com>
2026-02-25 14:44:22 +0400

    cli

--- source/cli.py
+++ source/cli.py
@@ -1,2 +1,34 @@
 """CLI interface."""
+import argparse
 
+
+class Command:
+    """Runner commands."""
+
+    Dataset = "dataset"
+
+
+PARSER = argparse.ArgumentParser(description='MicroGPT runner.')
+
+PARSER.add_argument(
+  "command",
+  choices=[Command.Dataset, ],
+  help="Command to execute."
+)
+PARSER.add_argument(
+  "filename",
+  help="Dataset file name."
+)
+
+def main(options):
+    """Entry point."""
+    print("MicroGPT runner.")
+
+    if options.command == Command.Dataset:
+        print("Dataset from", options.filename)
+
+    print("Done")
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main(PARSER.parse_args())
