83c890ac36f259aada537d31805d752eef7819b3
Vitaly Bogomolov <vit.sar68@gmail.com>
2026-02-27 09:49:16 +0400

    step

--- source/cli.py
+++ source/cli.py
@@ -8,28 +8,41 @@ class Command:
     """Runner commands."""
 
     Dataset = "dataset"
+    Learn = "learn"
 
 
 PARSER = argparse.ArgumentParser(description='MicroGPT runner.')
 
 PARSER.add_argument(
   "command",
-  choices=[Command.Dataset, ],
+  choices=[Command.Dataset, Command.Learn],
   help="Command to execute."
 )
 PARSER.add_argument(
   "filename",
   help="Dataset file name."
 )
+PARSER.add_argument(
+  "--learn_cycles",
+  type=int,
+  default=0,
+  help="Number of learn cycles (integer). Default is 0 - entire dataset content."
+)
 
 
 def main(options):
     """Entry point."""
     print("MicroGPT runner.")
+    data = Dataset(options.filename)
 
     if options.command == Command.Dataset:
-        data = Dataset(options.filename)
         print("Dataset from", data.file_name, "docs:", len(data.docs))
+
+    elif options.command == Command.Learn:
+        learn_cycles = len(data.docs)
+        if options.learn_cycles:
+            learn_cycles = max(options.learn_cycles, learn_cycles)
+        print("Learn {} docs from dataset {}".format(learn_cycles, data.file_name))
 
     print("Done")
 
